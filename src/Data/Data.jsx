b814fb907275f9ec2ffe8c7aa2000f27cff7ab8a
vinhsenpai <quangvinhdo21@gmail.com>
2024-11-26 21:16:37 +0700

    update main

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -12,6 +12,7 @@ const Movies = [
         "movieName": "Once Piece",
         "description": "One Piece là bộ truyện tranh dành cho thiếu niên của tác giả nổi tiếng Oda Eiichiro. Bộ manga này được chuyển thể thành một series anime nhiều tập hài hước, đặc sắc. Phim Đảo Hải Tặc mở đầu câu chuyện bằng cảnh xử tử vua hải tặc Gol D. Roger. Trước khi chết ông đã tiết lộ rằng mình có một kho báu được giấu ngoài biển. Nếu ai tìm thấy kho báu thì chúng sẽ thuộc về người đó. Rất nhiều người gan dạ đã đổ xô ra biển để tìm kiếm kho báu bí ẩn của vua hải tặc Gol D. Roger.",
         "image": "https://s3-alpha-sig.figma.com/img/6ea2/8ccf/1ea53edb90b65e5d33d48205a91a17a0?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=podOGasT6wgtO0UF5d7rkUVGP1bYyoGXw-2Qj0Sw5NGvEB3LrJclPAN7yYnzAbAUcnJoHFqvocCDjhOzZvz8ejDdxvdHKyIr8mvG9q3Jg0~xfnS6fFpg6myw5sFbayP9z9ZB5-fl8WpHtlyuGtxUdDQkU8TYOnWYz4t6X~5Ye1EXEMNYWVm3u9K065y3rp~0E-leHtZ34AT-xnFUaxeKydq5L4OdXDI0VHwCSTFo3oNsZbc5LPoE0kMJOCPXLb1OVlsv5fe0kOiGppDmeFqWMhthQsLO-iyXxFTzjmYDf5xV7kjv~Xb7Fd3IplUzKXCXyYaxIR4THY38ZPev-LdqVQ__",
+        "imageBanner":"https://i.pinimg.com/736x/ff/6e/b2/ff6eb2820802df2af1e20c85841eb907.jpg",
         "episode": "1018"
     },
     {
@@ -19,6 +20,7 @@ const Movies = [
         "movieName": "Boruto Naruto Next Generations",
         "description": "Boruto: Naruto Next Generations kể về thế hệ sau của Naruto. Bộ phim vẫn lấy bối cảnh là thế giới Ninja nhưng nhân vật chính lần này lại là Uzumaki Boruto, con trai của Uzumaki Naruto và Hyuga Hinata. Đồng hành cùng Naruto trong lần quay lại này là Sarada Uchiha là con gái của Sasuke Uchiha, Sakura Haruno và Mitsuki – “con” của Orochimaru.",
         "image": "https://s3-alpha-sig.figma.com/img/14e9/6cd4/177b17eb1ee85d84d09efcc0fa3e1b69?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jEzZGgKPOAnzOPWdb6q4MxFi6y9GiaXujRfBDJ-x9ipwp5o4mSDWou~ArRdnfbh9MUg6EPlhreyR-VZHwG0UjEs7wPQDZAv2PFd~RirtfHf4v-c6fs0pLkO3l-w~tRhObFFLEv2gvnHOepA8BM5OxNlvAgLZSFb~sCgL3bVWcxLhwUwktA1HjLBraJy2wfpbeV5XtTYd86uLRA~ja6fnDdIY72uQW9Q283LW5-rXvqAuJHB0N1XPa6Ddel7NlnTyHUqH2vXvnBD~JYOTYtc0igH1vV3Sz4wj-AKqB6SfFuw868QjfyefUaNffg1oVS2JDNOy66zhViz8QZB8msyqmg__",
+        "imageBanner":"https://i.pinimg.com/736x/6d/9c/4f/6d9c4fcf7cc72d582dcc340324235c5c.jpg",
         "episode": "250"
     },
     {
@@ -26,6 +28,7 @@ const Movies = [
         "movieName": "Spy X Family",
         "description": "Bộ phim là phần phim điện ảnh của series anime nổi tiếng Spy x Family. Trong bộ phim lần này, sau khi nhận được yêu cầu thay thế mình trong Chiến dịch Strix, Loid Forger quyết định giúp con gái Anya chiến thắng trong cuộc thi nấu ăn tại Học viện Eden bằng cách nấu bữa ăn yêu thích của hiệu trưởng để tránh bị thay thế khỏi nhiệm vụ mật. Nhưng từ đây, gia đình Forger phát hiện ra bí mật kinh hoàng ảnh hưởng đến hòa bình thế giới",
         "image": "https://s3-alpha-sig.figma.com/img/894b/a9f6/56cb8d734f17921555e3af4d5fbfd914?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aIGehtnanq5lTK6kmfxUFzIXwYByPdvlQhP3XoDnT-ujOXb4GnZnigxfkRah~TiAOUNhzBMwWrRXHlDmAkvVR5LEOfV-XGl6gw4nwoKgkNTpDvSCJYVVafoqQGVwpnoArXuwdhzJhzjbL90X-1tLWBJ6EEy3z9~Fmiu5U7J0Yl00xk~uepr5LaqxcvLfvdc5xc8mgXaIn853WvrbijPLQbPQzMmydWgkyHI0U3FuYQHu11IC2FqbilYC7xSBorWn0Q08mDBK9eq2v6B13ayt3ENtIlws6o6MoOeetIii4HnA2Lcam8dUZch-MFJ4ePh4XcfJrWh-NMQtQ~qkjx1hQQ__",
+        "imageBanner":"https://s3-alpha-sig.figma.com/img/a159/88d8/3feb9d7a97a93cac67eb291272ab227e?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f5k1WBYZxaoVkFTwI-94mq4J6wLwoJkTSotxhDEo04OGfnfMTI7MIAd-QLYnGbyTvHRkSlKmvlKgjht-883JhGw5GZa94KY3ibA-ZzNcjg-CZWrYvmFkpUs3Qo2pOu9MoaD78~SXSvvkDvnLUV2kJfyjs8lXuZ6z75CRXbawX9FCHGJ1MJBDf2U6iTcXhYA0-YcVWrN3PNIbWfsdINJ-lQfJYRin9ZoBOs2nZa-GRjJ4z0wN5KJXmVPQ-FdQC1anZJ98h92sNzcLo25bl7G1V6xgqJf111pgSGZbZa2Bu0k48dtU3CV0~pn39wvyZ6dlLfVCq2SvPPm4ggPONIeYxg__",
         "episode": "07"
     },
     {
@@ -33,6 +36,7 @@ const Movies = [
         "movieName": "Shingeki no kyoujin",
         "description": "Câu chuyện của Đại chiến Titan tập trung vào một nền văn minh bên trong ba bức tường đồ sộ quây tròn đồng tâm, nơi được cho là duy nhất mà nhân loại còn tồn tại. Ba bức tường được đặt tên theo tên ba người con gái của vị vua đầu tiên có tên là Maria, Rose và Sina. Người dân ở đó, được gọi là dân tộc Eldia, đã tin rằng hơn một trăm năm trước, loài người đã bị đẩy đến bờ vực tuyệt chủng sau sự xuất hiện của những người khổng lồ hình dáng con người, được gọi là Titan.",
         "image": "https://s3-alpha-sig.figma.com/img/f045/43fa/554deceb9b7c888745b5f43729a208d3?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CEEip4lGOimGiUgVpDk10-pKH8kcYPEb~qrcU9JzgmUGqI9gXfNFkRT83mwI~cKFXhEne~VijXv2wd0qBHzE63tZjr~QfP4LuMyExf-G-nSGaqDUPQdjQWeu6ejqG1hSzWjNdRDiJfywXuwAzqBmmpAYqE-kc3U104mowF-QtosqwMqqoSVQim0R6TLqoxmKnwg7mLEfD7OOKfjt6BHqgVBahSoQJeFruad8jl-lxO9rcPHv3sW~RKvI9L-af6Tb1SJHxLSbT7L7TFJPmyUuUK5fI3wEaoZhlA-1r8tgfJ8g4VmijxSBVHA9e5MQs81c4NZnmd4UQ2wZXxgiVejgSA__",
+        "imageBanner":"https://i.pinimg.com/736x/cf/91/78/cf91780300da4c2c81697c4f2662cc46.jpg",
         "episode": "28"
     },
     {
@@ -40,6 +44,7 @@ const Movies = [
         "movieName": "Captain Tsubasa",
         "description": "Tsubasa Oozora là nhân vật chính trong bộ truyện này. Khi còn nhỏ, cậu đã thoát chết nhờ một quả bóng bất ngờ. Điều đó đã thúc đẩy cậu trở thành một fan hâm mộ bóng đá. Câu chuyện tập trung vào các trận đấu bóng đá hấp dẫn, đem lại những bất ngờ cho khán giả. Đây là bản làm lại của 'Tsubasa Giấc mơ sân cỏ', với những chi tiết được thêm hoặc bỏ, để cộng thêm nét đặc trưng vào bộ truyện. Bộ phim này đã làm vợi lên kí ức về tuổi thơ của biết bao người.",
         "image": "https://s3-alpha-sig.figma.com/img/e5ab/b83d/eb0371790de015fd2dc503be277bd97b?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RvXhg0oIxy7aB3toFnSA6Igt9nAbEzCnq7gxv-218jbjK8aAY1da~dIRIYpsx~yU-k08uaEF~r7G51hsFwqnFUB1mPw6noz0BojWp88X8r-i-vPpjsgP8LAfKH5EV07p8ixXx4CTwnUHiw8sgZGV6GmjcBf5Y-JGenwCj5HA-NEPC9BEAZQh9vVl~ElkPJgxbAKp1Z5o9F5wHhIjpQGK2IpNwAfLiKiTNOjUrR7o0Go6sN4-cHx6-vWdMggePXFW9M04gLBv6DBEAycSj1pD5YCCcEQTN-4eNoXBRdAsVylM2HlGVwFoYD56dT8~bbxjPbLCw2YZPyI2P83bA4GNnA__",
+        "imageBanner":"https://i.pinimg.com/736x/ca/a2/66/caa266aecc32b9e2c735ad1a3e20a5ce.jpg",
         "episode": "28"
     },
     {
@@ -47,6 +52,7 @@ const Movies = [
         "movieName": "Ao Ashi",
         "description": "Ashito Aoi là một cầu thủ bóng đá trẻ, đầy khát vọng đến từ một thị trấn vùng nông thôn ở Nhật Bản. Hy vọng vào được một trường trung học với một câu lạc bộ bóng đá giỏi đã tan thành mây khói khi anh gây ra một sự cố trong một trận đấu quan trọng cho đội của mình, dẫn đến việc họ bị loại khỏi giải đấu. Tuy nhiên, anh đã lọt vào mắt xanh của một người quan trọng tình cờ đến thăm từ Tokyo. Mọi chuyện sẽ diễn ra như thế nào đối với Ashito?",
         "image": "https://s3-alpha-sig.figma.com/img/2fb6/7ff5/5e3abe50a16b45c46b38b61de4c3c1b2?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Nx33RfHaeJMGj3x5yYqSGL-l9ACm1M5jK4jUKT2kBxM6jgaSiUjHdmWQXrBcxEqgwAEvQ1SYKp6URV0rut54H-AU-5XiR24Dm4ZXH7bA65~JM8qfbeuQreBSmdiM3eyBYsqjAa~kj4CZKP2Irk4wRCChlRi-HO002tEzHmzkRh6vRd6zZp0~F4FUQlJAlUIJFdOxnIkqmhUGWKCVMYGm83ng6vZTMKh7H~jKsJMNCrhfzlt~Po5tZPxu37wSC~Qlal3SwJ53npmNYx3qZIhvjJLr4wC12xYoaBwO5ydjWz2scgzRSlmDRxXwF3pu-5mrEYTBZUwTODnYAQL~cA80GQ__",
+        "imageBanner":"https://i.pinimg.com/736x/49/70/1f/49701f5860e18a910e6df06d94913544.jpg",
         "episode": "28"
     }
 ]
--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -5,7 +5,7 @@ const Movie = (props) => {
     const {item} = props;
 
     const Clicked = () => {
-        document.getElementById("banner-img").src="https://s3-alpha-sig.figma.com/img/a159/88d8/3feb9d7a97a93cac67eb291272ab227e?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f5k1WBYZxaoVkFTwI-94mq4J6wLwoJkTSotxhDEo04OGfnfMTI7MIAd-QLYnGbyTvHRkSlKmvlKgjht-883JhGw5GZa94KY3ibA-ZzNcjg-CZWrYvmFkpUs3Qo2pOu9MoaD78~SXSvvkDvnLUV2kJfyjs8lXuZ6z75CRXbawX9FCHGJ1MJBDf2U6iTcXhYA0-YcVWrN3PNIbWfsdINJ-lQfJYRin9ZoBOs2nZa-GRjJ4z0wN5KJXmVPQ-FdQC1anZJ98h92sNzcLo25bl7G1V6xgqJf111pgSGZbZa2Bu0k48dtU3CV0~pn39wvyZ6dlLfVCq2SvPPm4ggPONIeYxg__"
+        document.getElementById("banner-img").src=item.imageBanner
         document.getElementById("banner-title").innerHTML=item.movieName
         document.getElementById("banner-text").innerHTML=item.description
 
